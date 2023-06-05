4d8f3c18aa6a6e38005d4ec6ec61399cb912fc37
Andrii Triukhan <39101979+em0nti@users.noreply.github.com>
2023-06-05 17:01:17 +0300

    Move logic to App component, del Feedback comp

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,9 @@
-import Feedback from './Feedback';
 import React, { Component } from 'react';
+import { Container } from 'react-bootstrap';
+import Statistics from './Statistics';
+import FeedbackOptions from './FeedbackOptions';
+import Section from './Section';
+import Notification from './Notification';
 
 class App extends Component {
   state = {
@@ -15,8 +19,35 @@ class App extends Component {
   };
 
   render() {
-    return <Feedback state={{...this.state}} onLeaveFeedback={this.onLeaveFeedback}/>;
+    const { good, neutral, bad } = this.state
+    const total = good + neutral + bad;
+    const positivePercentage =
+      total > 0 ? Math.round((good * 100) / total) : 0;
+
+  return (
+    <Container className="p-5">
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={Object.keys(this.state)}
+          onLeaveFeedback={this.onLeaveFeedback}
+        />
+      </Section>
+      <Section title="Statistics">
+        {total > 0 ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            positivePercentage={positivePercentage}
+          />
+        ) : (
+          <Notification message="There is no feedback given" />
+        )}
+      </Section>
+    </Container>
+  );
   }
-};
+}
 
 export default App;
